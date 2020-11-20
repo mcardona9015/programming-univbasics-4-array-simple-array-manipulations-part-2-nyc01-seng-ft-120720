c086d21bc31c5561f301846e197f406424e8a780
Michael Cardona <mcardona9015@gmail.com>
2020-11-20 20:08:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,3 +6,19 @@ end
 def using_insert(array, element)
   array.insert(4)
 end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, Integer)
+  array.delete_at(Integer)
+end
